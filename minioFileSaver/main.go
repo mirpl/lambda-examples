1812cf6d4ffcc790e7d3b0982c708f56d3b0457e
Antoni Czaplicki <antoni.czaplicki@makeitright.pl>
2020-06-07 01:07:52 +0200

    MVP-127 PR advices

--- minioFileSaver/main.go
+++ minioFileSaver/main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"path/filepath"
+	"path"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -18,8 +18,8 @@ import (
 )
 
 const (
-	S3Region = "us-east-1"
-	S3Bucket = "mvp-file-storage"
+	s3Region = "us-east-1"
+	s3Bucket = "mvp-file-storage"
 )
 
 var (
@@ -32,43 +32,49 @@ type fileSaverEvent struct {
 
 type fileSaverResponse struct {
 	inputURL string `json:"inputUrl"`
-	S3path   string `json:"s3path"`
+	s3Path   string `json:"s3Path"`
 }
 
-func getFileFromURL(requestURL string) (*os.File, error) {
-	if _, err := url.ParseRequestURI(requestURL); err != nil {
+func getFileFromURL(requestURL string) (*os.File, *url.URL, error) {
+	parsedURL, err := url.ParseRequestURI(requestURL)
+	if err != nil {
 		log.Error("parsing request URL failed", zap.Error(err))
-		return nil, err
+		return nil, nil, err
 	}
 
 	response, err := http.Get(requestURL)
 	if err != nil {
 		log.Error("getting URL response failed", zap.Error(err))
-		return nil, err
+		return nil, nil, err
 	}
 	defer response.Body.Close()
 
-	file, err := os.Create(filepath.Base(requestURL))
+	file, err := os.Create(path.Base(parsedURL.Path))
 	if err != nil {
 		log.Error("creating file failed", zap.Error(err))
-		return nil, err
+		return nil, nil, err
 	}
 
 	if _, err = io.Copy(file, response.Body); err != nil {
 		log.Error("dumping response body to file failed", zap.Error(err))
-		return nil, err
+		return nil, nil, err
 	}
-	return file, nil
+	return file, parsedURL, nil
 }
 
-func saveFileToS3(file *os.File, requestURL string) (string, error) {
-	s, err := session.NewSession(&aws.Config{Region: aws.String(S3Region)})
+func saveFileToS3(file *os.File, requestURL *url.URL) (string, error) {
+	s, err := session.NewSession(&aws.Config{Region: aws.String(s3Region)})
 	if err != nil {
-		log.Error("creating new S3 session failed", zap.Error(err))
+		log.Error("creating new AWS session failed", zap.Error(err))
 		return "", err
 	}
 
-	fileInfo, _ := file.Stat()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Error("getting file info failed", zap.Error(err))
+		return "", err
+
+	}
 	var size = fileInfo.Size()
 	buffer := make([]byte, size)
 	if _, err := file.Read(buffer); err != nil {
@@ -76,11 +82,11 @@ func saveFileToS3(file *os.File, requestURL string) (string, error) {
 		return "", err
 	}
 
-	s3path := fmt.Sprintf("s3.%s", requestURL)
+	s3KeyName := path.Base(requestURL.Path)
 	_, err = s3.New(s).PutObject(&s3.PutObjectInput{
-		Bucket:               aws.String(S3Bucket),
-		Key:                  aws.String(s3path),
-		ACL:                  aws.String("private"),
+		Bucket:               aws.String(s3Bucket),
+		Key:                  aws.String(s3KeyName),
+		ACL:                  aws.String("public"),
 		Body:                 bytes.NewReader(buffer),
 		ContentLength:        aws.Int64(size),
 		ContentType:          aws.String(http.DetectContentType(buffer)),
@@ -90,24 +96,26 @@ func saveFileToS3(file *os.File, requestURL string) (string, error) {
 	if err != nil {
 		log.Error("saving file to S3 failed", zap.Error(err))
 	}
-	return s3path, err
+	s3Path := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", s3Bucket, s3Region, s3KeyName)
+	return s3Path, err
 }
 
-func handler(ctx context.Context, evt fileSaverEvent) (fileSaverResponse, error) {
-	file, err := getFileFromURL(evt.requestURL)
+func handler(_ context.Context, evt fileSaverEvent) (fileSaverResponse, error) {
+
+	file, parsedURL, err := getFileFromURL(evt.requestURL)
 	if err != nil {
 		return fileSaverResponse{}, err
 	}
 	defer file.Close()
 
-	s3path, err := saveFileToS3(file, evt.requestURL)
+	s3Path, err := saveFileToS3(file, parsedURL)
 	if err != nil {
 		return fileSaverResponse{}, err
 	}
 
 	return fileSaverResponse{
 		inputURL: evt.requestURL,
-		S3path:   s3path,
+		s3Path:   s3Path,
 	}, nil
 }
 
